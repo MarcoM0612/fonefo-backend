114f5f6aa84cbddf0bda06b1bca3bc40d956563e
Nicolas Lopez <nicolaslopezcastiblanco@gmail.com>
2025-05-20 19:52:48 -0500

    Se realiza conexion local a base de datos

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,14 @@
 // paso 1: importamos la depedencia de express
 //const express = require('express') // commonJS
 import express from 'express'  // ESModule
+import product from './routes/product.route.mjs' //importamos las rutas de la entidad
 
 // paso 2: invocamos la ejecucuin de express
 const app = express()
 
-// paso 3: lanzamos el servidor web usando express en el puerto 3000
+app.use( product );         //implementar la ruta como middleware de express
+
+// paso 4: lanzamos el servidor web usando express en el puerto 3000
 app.listen( 3000, () => {
     console.log('seridor lanzado exitosamente')
 }  )
