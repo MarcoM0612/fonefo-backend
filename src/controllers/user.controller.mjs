f6bb6c947108603438658ea86cae9b14de6e8385
Marco Muñoz <marcoa.2132@gmail.com>
2025-05-26 21:56:25 -0500

    [+] Se establece la ruta delete y patch que envia la información al controlador.

--- src/controllers/user.controller.mjs
+++ src/controllers/user.controller.mjs
@@ -5,12 +5,10 @@ const createUser = async ( req,res) => {
     const inputData = req.body;
 
     // try: Controla las exepciones de la consolta a la base de datos 
-
     try {
         const registrerUser = await userModel.create( inputData );
-
-    console.log ( registrerUser );   //Imprime en la consola
-    res.status(201).json( registrerUser ); // Enviando la respuesta la cliente 
+        console.log ( registrerUser );   //Imprime en la consola
+        res.status(201).json( registrerUser ); // Enviando la respuesta la cliente 
     }  
     catch (error){ // catch: Captura el error producido por la excepcion
         console.log ( error );
@@ -24,9 +22,9 @@ const getAlluser = async ( req,res ) => {
 
     try {
         const data = await userModel.find({});
-    res.json( data );
-
-    } catch (error) {
+        res.json( data );
+    } 
+    catch (error) {
         console.error( error );
         res.json({ msg: 'Error: no se pudo obtener el listado de usuarios'})
     }
@@ -42,25 +40,55 @@ const getUserById = async ( req,res ) => {
         if ( ! data ) {
             return res.json({ msg: 'No se encuentra el usuario registrado'});
         }
-    res.json ( data );
+        res.json ( data );
     } 
     catch (error) {
         console.error( error );
         res.json({ msg: 'Error: no se pudo encontrar el registro'})
     }
 
+}
 
-    
-    
+const removeUserById = async ( req,res ) => {
+    const userId = req.params.id;
 
-
+    try {
+        const data =  await userModel.findByIdAndDelete ( userId )
+        //Verifica si el producto no existe y lanza el respoectivo mensaje al cliente
+        if ( ! data ) {
+            return res.json({ msg: 'El usuario no existe'});
+        }
+        res.json( data );
+    } 
+    catch (error) {
+        console.error( error );
+        res.json({ msg: 'Error: no se pudo encontrar el usuario'})
+    }
 
 }
+
+const updateUserById = async (req,res ) => {
+    const userId = req.params.id;      // Obtenemos el Id de la parametrización de la ruta 
+    const inputData = req.body;        // Obtenemos el Body de la petición
+
+    try {
+        const data = await userModel.findByIdAndUpdate( userId, inputData, { new: true} );
+    res.json ( data );
+    } 
+    catch (error) {
+        console.error( error );
+        res.json({ msg: 'Error: No se pudo actualizar el usuario'})
+    }
+
+}
+
 
 
 //Exponiendo las funcionalidades de este archivo usando el export
 export {
     createUser,
     getAlluser,
-    getUserById
+    getUserById,
+    removeUserById,
+    updateUserById
 }
--- src/routes/user.route.mjs
+++ src/routes/user.route.mjs
@@ -1,12 +1,14 @@
 import express from "express"; // importa la dependencia 
-import { createUser, getAlluser, getUserById } from "../controllers/user.controller.mjs";
+import { createUser, getAlluser, getUserById, removeUserById, updateUserById } from "../controllers/user.controller.mjs";
 
 const router = express.Router (); // Invoca el router de express
 
 //Definir ñas rutas para la entidad User 
 router.post( '/api/user', createUser);
 router.get('/api/user', getAlluser  );
-router.get('/api/user/:id', getUserById )             //:id (Parametrizar la ruta, se crea una especie de variable para que cualquier valor que pasemos despues de create.user lo tomo como un valor que espera la ruta)
+router.get('/api/user/:id', getUserById );             //:id (Parametrizar la ruta, se crea una especie de variable para que cualquier valor que pasemos despues de create.user lo tomo como un valor que espera la ruta)
+router.delete( '/api/user/:id', removeUserById);
+router.patch( '/api/user/:id', updateUserById );
 
 
 
