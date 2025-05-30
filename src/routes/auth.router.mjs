ab9773a7e1520ed6adea6db8cdb358eeadde435a
Marco Muñoz <marcoa.2132@gmail.com>
2025-05-29 19:51:02 -0500

    [+] auth.router - Define la ruta con el metodo post (api/register)

--- src/routes/auth.router.mjs
+++ src/routes/auth.router.mjs
@@ -1,8 +1,9 @@
 import express from "express";
+import { createUser } from "../controllers/user.controller.mjs";
 
 const router = express.Router ();
 
 //Define las rutas para la entidad Outh
-
+router.post('/api/register', createUser);
 
 export default router;
