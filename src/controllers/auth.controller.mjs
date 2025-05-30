c0b4095c32c44d1448b501229307acbef514e9e8
Marco Muñoz <marcoa.2132@gmail.com>
2025-05-29 21:39:36 -0500

    [+] npm i jsonwebtoken [+] auth.router.mjs: define la ruta POST api/login  [+] auth.controller

---
+++ src/controllers/auth.controller.mjs
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import userModel from "../schemas/user.schema.mjs";
+
+// Authentication 
+const loginUser = async(req, res ) => {
+    // Pasi 1: Obtener los datos del body
+    const inputData = req.body;
+    //Paso 2: Verificar si el usuario existe (por favor registrarse )
+    const userFound = await userModel.findOne({ email: inputData.email});
+    if(!userFound){
+        return res.json ({ msg: 'El usuario no existe, por favor registrese'});
+    }
+    //Paso 3: Verificar la contraseña (si cohinside)
+    const isAuthenticated = bcrypt.compareSync(
+        inputData.password,         //PASSWOR-ORIGINAL
+        userFound.password          //hash password
+    );
+    if(! isAuthenticated ){
+        return res.json({ msg: 'Contraseña invalida'});
+    }
+    //Paso 4: Generar credencial digital (Token)
+    const payload = {
+        name: userFound.name,
+        email: userFound.name,
+        role: userFound.role
+    }; 
+
+    const JWT_SECRET = '45644FDGSSFDHG74'
+
+    const token = jwt.sign(
+        payload,             //Carga util
+        JWT_SECRET,          //Palabra semilla o secreta 
+        { expiresIn: '1h' }  //Opciones de configuración del token
+    );
+
+    //paso 5: Eliminar algunas propiedades que traen datos sencibles 
+    const objsUser = userFound.toObject ();     //conertir un objeto Json en Objeto Javascript
+    delete objsUser.password;
+    delete objsUser.createdAt;
+    delete objsUser.updatedAt;
+
+    //Paso 6: respuesta al cliente 
+    res.json({
+        token, user:objsUser
+    });
+
+}
+
+export {
+    loginUser
+}
--- src/routes/auth.router.mjs
+++ src/routes/auth.router.mjs
@@ -1,9 +1,13 @@
 import express from "express";
 import { createUser } from "../controllers/user.controller.mjs";
+import { loginUser } from "../controllers/auth.controller.mjs";
+
+
 
 const router = express.Router ();
 
 //Define las rutas para la entidad Outh
 router.post('/api/register', createUser);
+router.post('/api/login', loginUser)
 
 export default router;
